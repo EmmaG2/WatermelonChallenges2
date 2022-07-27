76112f254c8a31ecaddae465a7a689088454c4b5
EmmanuelRdezG2 <itsmeemmadev@gmail.com>
2022-07-27 16:07:26 -0500

    reformat: tab bug solved

--- src/com/challenges/Week1.java
+++ src/com/challenges/Week1.java
@@ -13,129 +13,129 @@ import java.util.Scanner;
  */
 public class Week1
 {
-	public static String startDay1()
-	{
-		try
-		{
+  public static String startDay1()
+  {
+    try
+    {
 
-			System.out.println("Ingresa dos numeros:");
+      System.out.println( "Ingresa dos numeros:" );
 
-			int number1 = sc.nextInt();
-			int number2 = sc.nextInt();
+      int number1 = sc.nextInt();
+      int number2 = sc.nextInt();
 
-			if (number1 >= Math.pow(10, 5) || number2 >= Math.pow(10, 5))
-				return "mongolito";
+      if ( number1 >= Math.pow( 10, 5 ) || number2 >= Math.pow( 10, 5 ) )
+        return "mongolito";
 
-			sc.close();
+      sc.close();
 
-			return "Resultado: " + (number1 + number2);
-		}
-		catch (Exception e)
-		{
-			return "Error, vuelve a correr el programa";
-		}
-	}
+      return "Resultado: " + ( number1 + number2 );
+    }
+    catch ( Exception e )
+    {
+      return "Error, vuelve a correr el programa";
+    }
+  }
 
-	public static String startDay2()
-	{
-		try
-		{
-			int cantidadNinios, cantidadDulces, totalDulces;
+  public static String startDay2()
+  {
+    try
+    {
+      int cantidadNinios, cantidadDulces, totalDulces;
 
-			System.out.println("ninios y dulces: ");
+      System.out.println( "ninios y dulces: " );
 
-			cantidadDulces = sc.nextInt();
-			cantidadNinios = sc.nextInt();
-			totalDulces    = cantidadDulces / cantidadNinios;
+      cantidadDulces = sc.nextInt();
+      cantidadNinios = sc.nextInt();
+      totalDulces    = cantidadDulces / cantidadNinios;
 
-			if (totalDulces % 2 == 0)
-				return "Dulces: " + totalDulces;
+      if ( totalDulces % 2 == 0 )
+        return "Dulces: " + totalDulces;
 
-			sc.close();
+      sc.close();
 
-			return "No se puede: -1";
-		}
-		catch (Exception e)
-		{
-			return "Error, vuelve a correr el programa";
-		}
-	}
+      return "No se puede: -1";
+    }
+    catch ( Exception e )
+    {
+      return "Error, vuelve a correr el programa";
+    }
+  }
 
-	static public void startDay3() throws Exception
-	{
-		String input;
+  static public void startDay3() throws Exception
+  {
+    String input;
 
-		System.out.println("Input: ");
-		input = sc.nextLine();
+    System.out.println( "Input: " );
+    input = sc.nextLine();
 
-		sc.close();
+    sc.close();
 
-		System.out.println(input.length());
-	}
+    System.out.println( input.length() );
+  }
 
-	static public void startDay4() throws Exception
-	{
-		String stringOne, stringTwo;
+  static public void startDay4() throws Exception
+  {
+    String stringOne, stringTwo;
 
-		stringOne = sc.next();
-		stringTwo = sc.next();
+    stringOne = sc.next();
+    stringTwo = sc.next();
 
-		System.out.println(stringOne + " " + stringTwo);
-	}
+    System.out.println( stringOne + " " + stringTwo );
+  }
 
-	static public void startDay5() throws Exception
-	{
-		List<String> lista = new ArrayList<>();
+  static public void startDay5() throws Exception
+  {
+    List< String > lista = new ArrayList<>();
 
-		System.out.println("Lista Length:");
-		int listaLenght = sc.nextInt(), xParam;
+    System.out.println( "Lista Length:" );
+    int listaLenght = sc.nextInt(), xParam;
 
-		for (int i = 0; i < listaLenght; i++)
-		{
-			String inputElements = sc.next();
-			lista.add(inputElements);
-		}
+    for ( int i = 0; i < listaLenght; i++ )
+    {
+      String inputElements = sc.next();
+      lista.add( inputElements );
+    }
 
-		xParam = sc.nextInt();
+    xParam = sc.nextInt();
 
-		System.out.println(lista.get(xParam));
-	}
+    System.out.println( lista.get( xParam ) );
+  }
 
-	public static void startDay6()
-	{
-		String input;
+  public static void startDay6()
+  {
+    String input;
 
-		input = sc.next();
-		input = new StringBuilder(input).reverse().toString();
-		sc.close();
+    input = sc.next();
+    input = new StringBuilder( input ).reverse().toString();
+    sc.close();
 
-		System.out.println(input);
-	}
+    System.out.println( input );
+  }
 
-	public static void startDay7()
-	{
-		int cantidadElementos = sc.nextInt();
-		int cantidadMayores   = sc.nextInt();
+  public static void startDay7()
+  {
+    int cantidadElementos = sc.nextInt();
+    int cantidadMayores   = sc.nextInt();
 
-		System.out.println(logicDay7(cantidadElementos, cantidadMayores));
-	}
+    System.out.println( logicDay7( cantidadElementos, cantidadMayores ) );
+  }
 
-	private static int logicDay7(int cantidadElementos, int cantidadMayores)
-	{
-		int           result  = 0;
-		List<Integer> numeros = new ArrayList<>();
+  private static int logicDay7( int cantidadElementos, int cantidadMayores )
+  {
+    int             result  = 0;
+    List< Integer > numeros = new ArrayList<>();
 
-		for (int i = 0; i < cantidadElementos; i++)
-			numeros.add(sc.nextInt());
+    for ( int i = 0; i < cantidadElementos; i++ )
+      numeros.add( sc.nextInt() );
 
-		Collections.sort(numeros);
-		Collections.reverse(numeros);
+    Collections.sort( numeros );
+    Collections.reverse( numeros );
 
-		for (int i = 0; i < cantidadMayores; i++)
-			result += numeros.get(i);
+    for ( int i = 0; i < cantidadMayores; i++ )
+      result += numeros.get( i );
 
-		return result;
-	}
+    return result;
+  }
 
-	public static Scanner sc = new Scanner(System.in);
+  public static Scanner sc = new Scanner( System.in );
 }
--- src/com/challenges/Week2.java
+++ src/com/challenges/Week2.java
@@ -13,76 +13,76 @@ import java.util.stream.Collectors;
 
 public class Week2
 {
-	public static void startDay8()
-	{
-		int widthHeight = sc.nextInt();
-		int[][] array2D = new int[widthHeight][widthHeight];
+  public static void startDay8()
+  {
+    int widthHeight = sc.nextInt();
+    int[][] array2D = new int[widthHeight][widthHeight];
 
-		for (int i = 0; i < widthHeight; i++)
-			for (int j = 0; j < widthHeight; j++)
-				array2D[i][j] = sc.nextInt();
+    for ( int i = 0; i < widthHeight; i++ )
+      for ( int j = 0; j < widthHeight; j++ )
+        array2D[i][j] = sc.nextInt();
 
-		System.out.println(logicDay8(array2D) ? "YES" : "NO");
-	}
+    System.out.println( logicDay8( array2D ) ? "YES" : "NO" );
+  }
 
-	private static boolean logicDay8(int[][] matriz)
-	{
-		if (matriz[0][0] != 1)
-			for (int i = 0, j = matriz.length - 1; i < matriz.length; i++, j--)
-				if (matriz[i][j] != 1)
-					return false;
+  private static boolean logicDay8( int[][] matriz )
+  {
+    if ( matriz[0][0] != 1 )
+      for ( int i = 0, j = matriz.length - 1; i < matriz.length; i++, j-- )
+        if ( matriz[i][j] != 1 )
+          return false;
 
-		if (matriz[0][0] == 1)
-			for (int i = 0, j = 0; i < matriz.length; i++, j++)
-				if (matriz[i][j] != 1)
-					return false;
+    if ( matriz[0][0] == 1 )
+      for ( int i = 0, j = 0; i < matriz.length; i++, j++ )
+        if ( matriz[i][j] != 1 )
+          return false;
 
-		return true;
-	}
+    return true;
+  }
 
-	public static void startDay9()
-	{
-		Set<Integer> numbers = new TreeSet<>();
-		int          quantityNumbers, i;
+  public static void startDay9()
+  {
+    Set< Integer > numbers = new TreeSet<>();
+    int            quantityNumbers, i;
 
-		quantityNumbers = sc.nextInt();
+    quantityNumbers = sc.nextInt();
 
-		for (i = 0; i < quantityNumbers; i++)
-			numbers.add(sc.nextInt());
+    for ( i = 0; i < quantityNumbers; i++ )
+      numbers.add( sc.nextInt() );
 
-		numbers.forEach(n -> System.out.print(n + " "));
-	}
+    numbers.forEach( n -> System.out.print( n + " " ) );
+  }
 
-	/**
-	 *
-	 */
-	public static void startDay10()
-	{
-		Queue<Integer> cola = new LinkedList<Integer>();
-		Stack<Integer> pila = new Stack<>();
-		int            numberOfElements;
+  /**
+   *
+   */
+  public static void startDay10()
+  {
+    Queue< Integer > cola = new LinkedList< Integer >();
+    Stack< Integer > pila = new Stack<>();
+    int              numberOfElements;
 
-		numberOfElements = sc.nextInt();
+    numberOfElements = sc.nextInt();
 
-		for (int i = 0; i < numberOfElements; i++)
-		{
-			int numberToPush = sc.nextInt();
-			cola.add(numberToPush);
-			pila.push(numberToPush);
-		}
+    for ( int i = 0; i < numberOfElements; i++ )
+    {
+      int numberToPush = sc.nextInt();
+      cola.add( numberToPush );
+      pila.push( numberToPush );
+    }
 
-		System.out.println("Cola ->");
-		for (int i = 0; i < numberOfElements; i++)
-		{
-			System.out.print(cola.remove() + " ");
-		}
+    System.out.println( "Cola ->" );
+    for ( int i = 0; i < numberOfElements; i++ )
+    {
+      System.out.print( cola.remove() + " " );
+    }
 
-		System.out.println("\nStack ->");
-		for (int i = 0; i < numberOfElements; i++)
-		{
-			System.out.print(pila.pop() + " ");
-		}
-	}
+    System.out.println( "\nStack ->" );
+    for ( int i = 0; i < numberOfElements; i++ )
+    {
+      System.out.print( pila.pop() + " " );
+    }
+  }
 
-	public static Scanner sc = new Scanner(System.in);
+  public static Scanner sc = new Scanner( System.in );
 }
