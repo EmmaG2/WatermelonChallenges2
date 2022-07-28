9d339cec0be2cb5dee83761a34211478bc0dc925
EmmanuelRdezG2 <itsmeemmadev@gmail.com>
2022-07-28 14:01:07 -0500

    day 11 done

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,8 @@ import com.challenges.Week2;
 
 public class Main
 {
-  public static void main( String[] args ) throws Exception
+  public static void main(String[] args) throws Exception
   {
-    Week2.startDay10();
+    Week2.startDay11();
   }
 }
--- src/com/challenges/Week2.java
+++ src/com/challenges/Week2.java
@@ -1,15 +1,11 @@
 package com.challenges;
 
-import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.LinkedList;
-import java.util.List;
 import java.util.Queue;
 import java.util.Scanner;
 import java.util.Set;
 import java.util.Stack;
 import java.util.TreeSet;
-import java.util.stream.Collectors;
 
 public class Week2
 {
@@ -18,23 +14,23 @@ public class Week2
     int widthHeight = sc.nextInt();
     int[][] array2D = new int[widthHeight][widthHeight];
 
-    for ( int i = 0; i < widthHeight; i++ )
-      for ( int j = 0; j < widthHeight; j++ )
+    for (int i = 0; i < widthHeight; i++)
+      for (int j = 0; j < widthHeight; j++)
         array2D[i][j] = sc.nextInt();
 
-    System.out.println( logicDay8( array2D ) ? "YES" : "NO" );
+    System.out.println(logicDay8(array2D) ? "YES" : "NO");
   }
 
-  private static boolean logicDay8( int[][] matriz )
+  private static boolean logicDay8(int[][] matriz)
   {
-    if ( matriz[0][0] != 1 )
-      for ( int i = 0, j = matriz.length - 1; i < matriz.length; i++, j-- )
-        if ( matriz[i][j] != 1 )
+    if (matriz[0][0] != 1)
+      for (int i = 0, j = matriz.length - 1; i < matriz.length; i++, j--)
+        if (matriz[i][j] != 1)
           return false;
 
-    if ( matriz[0][0] == 1 )
-      for ( int i = 0, j = 0; i < matriz.length; i++, j++ )
-        if ( matriz[i][j] != 1 )
+    if (matriz[0][0] == 1)
+      for (int i = 0, j = 0; i < matriz.length; i++, j++)
+        if (matriz[i][j] != 1)
           return false;
 
     return true;
@@ -42,15 +38,15 @@ public class Week2
 
   public static void startDay9()
   {
-    Set< Integer > numbers = new TreeSet<>();
-    int            quantityNumbers, i;
+    Set<Integer> numbers = new TreeSet<>();
+    int          quantityNumbers, i;
 
     quantityNumbers = sc.nextInt();
 
-    for ( i = 0; i < quantityNumbers; i++ )
-      numbers.add( sc.nextInt() );
+    for (i = 0; i < quantityNumbers; i++)
+      numbers.add(sc.nextInt());
 
-    numbers.forEach( n -> System.out.print( n + " " ) );
+    numbers.forEach(n -> System.out.print(n + " "));
   }
 
   /**
@@ -58,31 +54,40 @@ public class Week2
    */
   public static void startDay10()
   {
-    Queue< Integer > cola = new LinkedList< Integer >();
-    Stack< Integer > pila = new Stack<>();
-    int              numberOfElements;
+    Queue<Integer> cola = new LinkedList<Integer>();
+    Stack<Integer> pila = new Stack<>();
+    int            numberOfElements;
 
     numberOfElements = sc.nextInt();
 
-    for ( int i = 0; i < numberOfElements; i++ )
+    for (int i = 0; i < numberOfElements; i++)
     {
       int numberToPush = sc.nextInt();
-      cola.add( numberToPush );
-      pila.push( numberToPush );
+      cola.add(numberToPush);
+      pila.push(numberToPush);
     }
 
-    System.out.println( "Cola ->" );
-    for ( int i = 0; i < numberOfElements; i++ )
+    System.out.println("Cola ->");
+    for (int i = 0; i < numberOfElements; i++)
     {
-      System.out.print( cola.remove() + " " );
+      System.out.print(cola.remove() + " ");
     }
 
-    System.out.println( "\nStack ->" );
-    for ( int i = 0; i < numberOfElements; i++ )
+    System.out.println("\nStack ->");
+    for (int i = 0; i < numberOfElements; i++)
     {
-      System.out.print( pila.pop() + " " );
+      System.out.print(pila.pop() + " ");
     }
   }
 
-  public static Scanner sc = new Scanner( System.in );
+  public static void startDay11()
+  {
+    int userNumberInput;
+    userNumberInput = sc.nextInt();
+    char output     = (char)userNumberInput;
+
+    System.out.println(output);
+  }
+
+  public static Scanner sc = new Scanner(System.in);
 }
