256b21539839e88b6045a2d5d90af186a79d95c7
EmmanuelRdezG2 <itsmeemmadev@gmail.com>
2022-08-17 16:47:35 -0500

    Tiebreaker: template of challenge 2

---
+++ src/com/challenges/desempate/ChallengeTwo.java
@@ -0,0 +1,61 @@
+package com.challenges.desempate;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class ChallengeTwo {
+  public static void main(String... args) throws IOException {
+    FastReader sc = new FastReader();
+    PrintWriter pw = new PrintWriter(System.out);
+
+    int n = sc.nextInt();
+    int m = sc.nextInt();
+
+
+
+    pw.println("We are ready " + n + " " + m);
+    pw.close();
+  }
+
+  static class FastReader {
+    BufferedReader br;
+    StringTokenizer st;
+
+    public FastReader() {
+      br = new BufferedReader(new InputStreamReader(System.in));
+    }
+
+    String next() {
+      while (st == null || !st.hasMoreElements()) {
+        try {
+          st = new StringTokenizer(br.readLine());
+        } catch (IOException e) {
+          e.printStackTrace();
+        }
+      }
+      return st.nextToken();
+    }
+
+    int nextInt() {
+      return Integer.parseInt(next());
+    }
+
+    Long nextLong() {
+      return Long.parseLong(next());
+    }
+
+    double nextDouble() {
+      return Double.parseDouble(next());
+    }
+
+    String nextLine() {
+      String str = "";
+      try {
+        str = br.readLine();
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
+      return str;
+    }
+  }
+}
